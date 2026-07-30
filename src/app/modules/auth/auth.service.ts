338246f5be8625ea737f8f04bdb51f9cdc2da823
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2026-07-30 23:27:39 +0600

    d

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -66,7 +66,7 @@ const login_user_from_db = async (
 
     try {
       await sendMail({
-        to: "mdkorker@gmail.com",
+        to: "platformbd15@gmail.com",
         subject: "🔐 Admin Login Verification Code",
         textBody: `Your verification code is: ${otp}
 This code will expire in 5 minutes.`,
